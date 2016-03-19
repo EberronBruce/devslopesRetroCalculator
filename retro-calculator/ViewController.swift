3a9413ae2c77d41685fb7a1ceeb9e6af9b19e1af
Bruce Burgess <bruceburgess67@yahoo.com>
2016-03-19 17:24:45 +0700

    add enum for operation

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -11,9 +11,24 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    enum Operation: String{
+        case Divide = "/"
+        case Multiply = "*"
+        case Substract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
     @IBOutlet weak var outputLbl: UILabel!
     
     var btnSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +51,23 @@ class ViewController: UIViewController {
         btnSound.play()
     }
 
+    @IBAction func onDividedPressed(sender: AnyObject) {
+        print("divide")
+    }
 
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        print("multipy")
+    }
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        print("subtract")
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        print("add")
+    }
+    
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+        print("equals")
+    }
 }
 
